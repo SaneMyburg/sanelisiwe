ef2647d98de1b1bf5cf057c2b3be266772409e88
SaneMyburg <sane.myburg@gmail.com>
2023-10-19 08:49:26 +0200

    Add style to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,26 +18,28 @@ const Navigation = () => {
   };
 
   return (
-    <Navbar expand="lg" variant="light" bg="light" className="header">
-      <div className="header-right">
-        <Navbar.Brand className="logo" href="/">S</Navbar.Brand>
-        <Navbar.Brand className="name" href="/">Sane Myburg</Navbar.Brand>
-      </div>
-      <div className="header-left">
-        <Navbar.Toggle className="show-hamburger" onClick={toggleMenu}>
-          <HamburgerIcon />
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className={`mr-auto ${showLinks ? 'show-hamburger' : 'hide-links'}`}>
-            {links.map((link) => (
-              <Nav.Link as={NavLink} to={link.path} key={link.text}>
-                {link.text}
-              </Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+    <div>
+      <Navbar expand="lg" variant="light" bg="light" className="header">
+        <div className="header-right">
+          <Navbar.Brand className="logo" href="/">S</Navbar.Brand>
+          <Navbar.Brand className="name" href="/">Sane Myburg</Navbar.Brand>
+        </div>
+        <div className="header-left">
+          <Navbar.Toggle className="show-hamburger" onClick={toggleMenu}>
+            <HamburgerIcon />
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className={`mr-auto ${showLinks ? 'show-hamburger' : 'hide-links'}`}>
+              {links.map((link) => (
+                <Nav.Link as={NavLink} to={link.path} key={link.text} className="nav-link">
+                  {link.text}
+                </Nav.Link>
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
   );
 };
 
